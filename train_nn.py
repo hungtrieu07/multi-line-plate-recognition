6a04d85b56823f6af007aeb8ad9d6c4e086761b7
hungtrieu07 <hungtrieu0701@gmail.com>
2022-09-17 10:13:58 +0700

    Dmodify

--- train_nn.py
+++ train_nn.py
@@ -1,9 +1,9 @@
-import tensorflow.python.keras.backend as K
-from tensorflow.python.keras.layers import *
-from tensorflow.python.keras.layers import ZeroPadding2D, MaxPooling2D
-from tensorflow.python.keras.layers import Lambda
-from tensorflow.python.keras.models import Model
-from tensorflow.python.keras.callbacks import Callback
+import tensorflow.keras.backend as K
+from tensorflow.keras.layers import *
+from tensorflow.keras.layers import ZeroPadding2D, MaxPooling2D
+from tensorflow.keras.layers import Lambda
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import Callback
 import numpy as np
 import os
 import tensorflow as tf
@@ -155,7 +155,7 @@ if __name__ == '__main__':
     model.summary()
     history = LossHistory()
 
-    data,label = gen_data_label_data(True)
+    data,label = gen_data_label_data()
 
     input_length = np.zeros([len(data),1])
     label_length = np.zeros([len(data),1])
